35dcfa131171a758530f0860fc39d9bff471d85a
ssabrut <michael.gunawan2002@gmail.com>
2022-03-28 20:09:55 +0700

    Refactor minimax algo

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 class Tictactoe:
     player, bot = '', ''
-    redo = True
 
     def __init__(self, player, bot):
         self.player = player
         self.bot = bot
-
-    def header(self):
-        print('================================================')
-        print('\tWelcome to Minimax Tictactoe!')
-        print('================================================')
 
     def display_board(self, board):
         for i in range(0, len(board)):
@@ -17,131 +11,141 @@ class Tictactoe:
                 print(board[i][j], end='')
             print('')
 
-    def check_moves(self, board):
+    def available_moves(self, board, number):
         for i in range(0, len(board), 2):
-            for j in range(0, len(board), 2):
+            for j in range(0, len(board[i]), 2):
                 if type(board[i][j]) == int:
-                    return True
+                    if board[i][j] == number:
+                        return True
         return False
 
-    def evaluate_board(self, board):
-        # checking for row
+    def get_number_position(self, board, number):
+        for i in range(0, len(board), 2):
+            for j in range(0, len(board[i]), 2):
+                if board[i][j] == number:
+                    return (i, j)
+        return (-1, -1)
+
+    def tie(self, board):
+        for i in range(0, len(board), 2):
+            for j in range(0, len(board[i]), 2):
+                if type(board[i][j]) == int:
+                    return False
+        return True
+
+    def evaluate_board(self, board, label=''):
+        #check row
         for i in range(0, len(board), 2):
             if board[i][0] == board[i][2] and board[i][2] == board[i][4]:
-                if board[i][0] == self.bot:
-                    return 10
-                elif board[i][0] == self.player:
-                    return -10
+                return True
+            if board[i][0] == board[i][2] and board[i][2] == board[i][4] and board[i][0] == label:
+                return True
 
-        # checking for column
+        #check column
         for i in range(0, len(board), 2):
             if board[0][i] == board[2][i] and board[2][i] == board[4][i]:
-                if board[0][i] == self.bot:
-                    return 10
-                elif board[0][i] == self.player:
-                    return -10
+                return True
+            if board[0][i] == board[2][i] and board[2][i] == board[4][i] and board[0][i] == label:
+                return True
 
-        # left diagonal
-        left = 0
-        if board[left][left] == board[left + 2][left + 2] and board[left + 2][left + 2] == board[left + 4][left + 4]:
-            if board[left][left] == self.bot:
-                return 10
-            elif board[left][left] == self.player:
-                return -10
+        #left diagonal
+        if board[0][0] == board[2][2] and board[2][2] == board[4][4]:
+            return True
+        if board[0][0] == board[2][2] and board[2][2] == board[4][4] and board[0][0] == label:
+            return True
 
-        # right diagonal
-        right = 0
-        if board[right + 4][right] == board[right + 2][right + 2] and board[right + 2][right + 2] == board[right][right + 4]:
-            if board[right][right] == self.bot:
-                return 10
-            elif board[right][right] == self.player:
-                return -10
+        #right diagonal
+        if board[4][0] == board[2][2] and board[2][2] == board[0][4]:
+            return True
+        if board[4][0] == board[2][2] and board[2][2] == board[0][4] and board[4][0] == label:
+            return True
+        return False
 
-        return 0
+    def put_label(self, board, number, label):
+        if self.available_moves(board, number):
+            x, y = self.get_number_position(board, number)
+            board[x][y] = label
+            self.display_board(board)
+            if self.tie(board):
+                print("=== TIE ===")
+                exit()
+            if self.evaluate_board(board):
+                if label == self.player:
+                    print("=== YOU WIN ===")
+                    exit()
+                elif label == self.bot:
+                    print("=== AI WIN ===")
+                    exit()
+            return
+        else:
+            print(f"Cant put label on number {number}")
+            number = int(input("Enter new position: "))
+            self.put_label(board, number, label)
+            return
 
     def minimax(self, board, depth, is_maximum):
-        score = self.evaluate_board(board)
-        if score == 1 or score == -1:
-            return score
-
-        if not self.check_moves(board):
+        if self.evaluate_board(board, self.bot):
+            return 1
+        elif self.evaluate_board(board, self.player):
+            return -1
+        elif self.tie(board):
             return 0
 
         if is_maximum:
-            best = -10
+            best_score = -1
             for i in range(0, len(board), 2):
-                for j in range(0, len(board), 2):
+                for j in range(0, len(board[i]), 2):
                     if type(board[i][j]) == int:
-                        temp = board[i][j]
+                        value = board[i][j]
                         board[i][j] = self.bot
-                        best = max(best, self.minimax(board, depth + 1, not is_maximum))
-                        board[i][j] = temp
-            return best
+                        score = max(best_score, self.minimax(board, depth + 1, False))
+                        board[i][j] = value
+                        if score > best_score:
+                            best_score = score
+            return best_score
         else:
-            best = 10
+            best_score = 1
             for i in range(0, len(board), 2):
-                for j in range(0, len(board), 2):
+                for j in range(0, len(board[i]), 2):
                     if type(board[i][j]) == int:
-                        temp = board[i][j]
+                        value = board[i][j]
                         board[i][j] = self.player
-                        best = min(best, self.minimax(board, depth + 1, not is_maximum))
-                        board[i][j] = temp
-            return best
-
-    def find_best_move(self, board):
-        best_value = -100
-        best_move = (-1, -1)
-        for i in range(0, len(board), 2):
-            for j in range(0, len(board), 2):
-                if type(board[i][j]) == int:
-                    temp = board[i][j]
-                    board[i][j] = self.bot
-                    move = self.minimax(board, 0, False)
-                    board[i][j] = temp
-                    if move > best_value:
-                        best_move = (i, j)
-                        best_value = move
-        return best_move
+                        score = min(best_score, self.minimax(board, depth + 1, True))
+                        board[i][j] = value
+                        if score < best_score:
+                            best_score = score
+            return best_score
 
     def player_turn(self, board):
-        print('Your turn!')
         self.display_board(board)
-        temp = input('Choose between 1-9 from the board: ')
-        try:
-            player_input = int(temp)
-            for i in range(0, len(board), 2):
-                for j in range(0, len(board), 2):
-                    if type(board[i][j]) == int:
-                        if board[i][j] == player_input:
-                            board[i][j] = self.player
-                            print(f'You put {self.player} on board number {player_input}!\n')
-                            score = self.evaluate_board(board)
-                            if score != 0:
-                                self.redo = not self.redo
-                                print('You win!')
-        except ValueError:
-            print('Choose only number from the board!')
+        position = int(input("Choose between 1-9 from the board: "))
+        self.put_label(board, position, self.player)
+        return
 
-    def bot_turn(self, board):
-        print('Bot turn!')
-        self.display_board(board)
-        best_move = self.find_best_move(board)
-        row = best_move[0]
-        col = best_move[1]
-        number = board[row][col]
-        if type(board[row][col]) == int:
-            board[row][col] = self.bot
-            print(f'Bot put {self.bot} on board number {number}!\n')
+    def ai_turn(self, board):
+        best_score = -1
+        best_move = 0
+        for i in range(0, len(board), 2):
+            for j in range(0, len(board[i]), 2):
+                if type(board[i][j]) == int:
+                    value = board[i][j]
+                    board[i][j] = self.bot
+                    score = self.minimax(board, 0, False)
+                    board[i][j] = value
+                    if score > best_score:
+                        best_score = score
+                        best_move = value
+        self.put_label(board, best_move, self.bot)
+        return
 
     def start(self, board):
-        self.header()
-        if self.player == 'x':
-            while self.redo:
+        if self.player == 'x'.upper():
+            while not self.evaluate_board(board):
                 self.player_turn(board)
-                self.bot_turn(board)
-        elif self.player == 'o':
-            while True:
-                self.bot_turn(board)
+                self.ai_turn(board)
+        elif self.player == 'o'.upper():
+            while not self.evaluate_board(board):
+                self.ai_turn(board)
                 self.player_turn(board)
 
 if __name__ == '__main__':
@@ -164,5 +168,5 @@ if __name__ == '__main__':
                 bot = 'o'
             elif player.lower() == 'o':
                 bot = 'x'
-    tic = Tictactoe(player.lower(), bot.lower())
+    tic = Tictactoe(player.upper(), bot.upper())
     tic.start(board)
